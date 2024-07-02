83ea53317b8b5a9f83e2492bf6de1e694715aed8
Ruben Servando <servandostudio@gmail.com>
2024-07-02 21:25:30 +0100

    Fix Login and Register Modals footer

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -24,8 +24,10 @@ import { useRouter } from 'next/navigation';
 
 const LoginModal = () => {
     const router = useRouter();
+
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
+
     const [isLoading, setIsLoading] = useState(false);
 
     const {
@@ -62,6 +64,11 @@ const LoginModal = () => {
             }
         })
     }
+
+    const toggle = useCallback(() => {
+        loginModal.onClose();
+        registerModal.onOpen();
+    }, [loginModal, registerModal]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
@@ -120,17 +127,17 @@ const LoginModal = () => {
                 "
                 >
                     <div>
-                        Already have an account?
+                        First time using Yaquevas?
                     </div>
                     <div
-                        onClick={registerModal.onClose}
+                        onClick={toggle}
                         className="
                             text-neutral-800
                             cursor-pointer
                             hover:underline
                         "
                     >
-                        Log in
+                        Create an account
                     </div>
                 </div>
             </div>

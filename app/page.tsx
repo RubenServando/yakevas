14ec4af7d51199da123d4ea421de444f526487d2
Ruben Servando <servandostudio@gmail.com>
2024-07-07 14:23:34 +0100

    Fix home page color

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <div className="text-rose-500 text-2xl">Hello yakevas!!</div>
+    <div className="text-yellow-400 text-2xl">Hello yakevas!!</div>
   );
 }
